b50fbde6fc38ef366466ec21e3df59cf6c80796c
muradaghazade <xomurad41@gmail.com>
2025-04-05 10:23:14 +0400

    m

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to='images/')
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
